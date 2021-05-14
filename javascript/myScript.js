aa0b352d560fa4a989ee91b9fd64cf10cde8210c
kushbhandari <kushbhandari54@gmail.com>
2021-05-14 08:57:06 +0530

    First commit

---
+++ javascript/myScript.js
@@ -0,0 +1,12 @@
+function myFunction()
+{
+    document.getElementById("demo").innerHTML="Paragraph Changed.";
+    document.getElementById("p1").style.fontSize="35";
+    document.getElementById("p2").style.backgroundColor="blue";
+    document.getElementById("p3").style.color="red";
+
+}
+function change()
+{
+    alert("Hi,how are you?");
+}
